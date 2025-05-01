06f1e0bbafd9be0ca7d59de62d5f62c7c3ca701a
Davide Marchegiani <davide.marchegiani@anu.edu.au>
2025-05-01 12:02:06 +1000

    Fixed access to config inputs in plugin

--- src/include_configuration_stubs/__init__.py
+++ src/include_configuration_stubs/__init__.py
@@ -1,9 +1,8 @@
 from . import _version
 from include_configuration_stubs.utils import check_is_installed
 
-REQUIRED_EXECS = [
-    "git"
-]
+PLUGIN_NAME = "mkdocs-include-configuration-stubs-plugin"
+REQUIRED_EXECS = ["git"]
 
 for exe in REQUIRED_EXECS:
     check_is_installed(exe)
--- src/include_configuration_stubs/plugin.py
+++ src/include_configuration_stubs/plugin.py
@@ -5,62 +5,72 @@ import os
 from mkdocs.plugins import BasePlugin
 from mkdocs.structure.files import File
 
+from include_configuration_stubs import PLUGIN_NAME
 from include_configuration_stubs.config import ConfigScheme
 from include_configuration_stubs.utils import (
     get_config_stub,
     get_git_refs,
     get_repo_from_input,
-    get_supported_file_formats,
     is_main_website,
 )
 
 
 class IncludeConfigurationStubsPlugin(BasePlugin[ConfigScheme]):
     def on_config(self, config):
-        self.supported_file_formats = get_supported_file_formats(
-            config.supported_file_formats
+        self.repo = get_repo_from_input(self.config["repo"])
+        self.is_main_website = is_main_website(
+            self.config["main_website"]["branch"], self.repo
         )
-        self.repo = get_repo_from_input(config.repo)
-        self.is_main_website = is_main_website(config.main_website.branch, self.repo)
 
     def on_files(self, files, config):
         """Hook to modify the files."""
         # Add the configuration stubs to the site
         repo = self.repo
         is_main_website = self.is_main_website
-        website = config.main_website if is_main_website else config.preview_website
+        main_website_config = self.config["main_website"]
+        preview_website_config = self.config["preview_website"]
+        website = main_website_config if is_main_website else preview_website_config
         # Add stubs to the site
         refs = get_git_refs(
             repo,
-            pattern=website.pattern,
-            ref_type=website.ref_type,
+            pattern=website["pattern"],
+            ref_type=website["ref_type"],
         )
         # If is a preview website and 'no_main' is False, include also the main website stubs
-        if not is_main_website and not config.preview_website.no_main:
+        if not is_main_website and not preview_website_config["no_main"]:
             refs.extend(
                 get_git_refs(
                     repo,
-                    pattern=config.main_website.pattern,
-                    ref_type=config.main_website.ref_type,
+                    pattern=main_website_config["pattern"],
+                    ref_type=main_website_config["ref_type"],
                 )
             )
         # Remove duplicate refs
         refs = set(refs)
         # For each ref, add its configuration stubs to the site, if present
+        stubs_dir = self.config["stubs_dir"]
+        stubs_site_dir = self.config["stubs_site_dir"]
+        docs_dir = config["docs_dir"]
+        site_dir = config["site_dir"]
+        use_directory_urls = config["use_directory_urls"]
         for ref in refs:
             config_stub = get_config_stub(
-                ref, repo, config.stubs_dir, self.supported_file_formats
+                ref,
+                repo,
+                stubs_dir,
+                self.config["supported_file_formats"],
             )
             if config_stub is not None:
                 fname = next(iter(config_stub))
                 config_stub_file = File(
                     path=fname,
-                    src_dir=os.path.join(config["docs_dir"], config["stubs_dir"]),
+                    src_dir=os.path.join(docs_dir, stubs_dir),
                     dest_dir=os.path.join(
-                        config["site_dir"], config["stubs_website_dir"]
+                        site_dir,
+                        stubs_site_dir,
                     ),
-                    use_directory_urls=config["use_directory_urls"],
-                    generated_by="mkdocs-include-configuration-stubs-plugin",
+                    use_directory_urls=use_directory_urls,
+                    generated_by=PLUGIN_NAME,
                 )
                 config_stub_file.content_string = config_stub[fname]
                 files.append(config_stub_file)
